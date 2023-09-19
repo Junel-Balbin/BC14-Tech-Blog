130d92ea33ada95afd87956efe1b740a93f75aee
Junel <robin.lennuj@gmail.com>
2023-09-19 11:24:37 -0700

    Worded on the server.js.

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+const routes = require('./controllers');
+const expressHandlebars = require('express-handlebars');
+
+const helpers = require("./utils/helpers");
+const path = require("path");
+
+const session = require('express-session');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Create handlebar instance.
+const hbsengine = expressHandlebars.create({
+  helpers,
+});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Session configuration.
+const sessionObj = {
+  secret: process.env.SECRET_KEY,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}; 
+
+app.use(session(sessionObj));
+
+app.engine('handlebars', hbsengine.engine);
+app.set('view engine', 'handlebars');
+
+// Middleware setup.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "public")));
+
+// Prevents page caching on browser to protect from back button navigation into protected resource after logout.
+app.use(function (req, res, next) {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  next()
+});
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
+// Mini project 14 code structure & code snippets.

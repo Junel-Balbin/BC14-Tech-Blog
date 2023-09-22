0871cf5eb8697b1f5cf1e8bffcd6b9496857ecf2
Junel <robin.lennuj@gmail.com>
2023-09-22 11:03:04 -0700

    Made some changes & additions to userRoutes.js & index.js.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
-router.use('/user', userRoutes);
+router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
+
+
+// Template Structure and Code Snippets from Mini Project 14.

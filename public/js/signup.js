3aed01c585fba36869afc67449f74486d7a67fbc
Junel <robin.lennuj@gmail.com>
2023-09-27 20:13:35 -0700

    Fixed issues with Sign Up feature. Now working.

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,10 @@
 document.getElementById("createForm").addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const username = document.getElementById("create_username").value.trim();
-    const email = document.getElementById("create_email").value.trim();
+    const full_name = document.getElementById("create_name").value.trim();
+    const name = document.getElementById("create_username").value.trim();
     const password = document.getElementById("create_password").value.trim();
+    const email = document.getElementById("create_email").value.trim();
 
     fetch('/api/users', {
         method: 'POST',
@@ -11,19 +12,22 @@ document.getElementById("createForm").addEventListener("submit", (e) => {
             'content-type': 'application/json',
         },
         body: JSON.stringify(  {
-            "username": username,
+            "name": full_name,
+            "username": name,
             "email": email,
             "password": password
           }),
     }).then((result) => {
         if (!result.ok) {
-            alert("Invalid username, email or password ");
+            alert("Unsuccessful Sign Up.");
             return;
         }
 
         result.json().then(() => {
             window.location = '/';
-            
         });
     });
 }); 
+
+
+// Template Structure and Code Snippets from Mini Project 14.
